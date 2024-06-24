9a2cab194ded4698dc40dfc2bdb505d3c62a3274
ticepw <156025162+ticepw@users.noreply.github.com>
2024-06-23 20:11:59 -0400

    created a counter

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+
+
+  return (
+    <div className="App">
+     <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+     <h1>{counter}</h1>
+     <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
